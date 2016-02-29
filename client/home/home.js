601e9be96eaf8f9a9b2ca3477844f97025601d86
George Allen <george.allen93@outlook.com>
2016-02-29 22:49:27 +0000

    Added all files from old portfolio.

--- client/home/home.js
+++ client/home/home.js
@@ -1,10 +1,28 @@
 /**
  * Created by georgeallen on 29/02/2016.
  */
-angular.module('portfolio').controller('HomeController', ['$scope', '$log', '$reactive',
-    function ($scope, $log, $reactive) {
+angular.module('portfolio').controller('HomeController', ['$scope', '$log', '$reactive', '$window', '$anchorScroll', '$location',
+    function ($scope, $log, $reactive, $window, $anchorScroll, $location) {
         $reactive(this).attach($scope);
 
-        this.message = 'Hello Portfolio';
+        this.browserHeight = $window.innerHeight;
+
+        $(window).on("resize.doResize", function () {
+            this.browserHeight = $window.innerHeight;
+            console.log("hello");
+            $scope.$apply();
+        });
+
+        $scope.$on("$destroy", function () {
+           $(window).off("resize.doResize");
+        });
+
+        this.goToAnchor = function(scrollId) {
+            if($location.hash() !== scrollId){
+                $location.hash(scrollId);
+            } else {
+                $anchorScroll();
+            }
+        };
 
     }]);
